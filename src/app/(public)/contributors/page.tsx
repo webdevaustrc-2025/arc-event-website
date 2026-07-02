53858d1e134d70cf73ef50a83ea038b698fd9a5d
ssirajussalikin119 <ssirajussalikin98@gmail.com>
2026-07-03 03:19:49 +0600

    feat: add contributors page with profile cards, images, and social links

---
+++ src/app/(public)/contributors/page.tsx
@@ -0,0 +1,10 @@
+import ContributorsPage from "@/components/pages/ContributorsPage";
+
+export const metadata = {
+    title: "Contributors | AUSTRC ARC 3.0",
+    description: "Meet the talented team of contributors who built and designed the ARC 3.0 event platform.",
+};
+
+export default function Page() {
+    return <ContributorsPage />;
+}
---
+++ src/data/contributorsData.ts
@@ -0,0 +1,75 @@
+export interface Contributor {
+  id: number;
+  name: string;
+  designation: string;
+  email: string;
+  linkedin: string;
+  facebook: string;
+  github: string;
+  imageUrl: string;
+}
+
+export const contributorsData: Contributor[] = [
+  // Director (Top)
+  {
+    id: 1,
+    name: "Saobia Islam Tinni",
+    designation: "Director",
+    email: "islamsaobia@gmail.com",
+    linkedin: "https://www.linkedin.com/in",
+    facebook: "https://www.facebook.com/sag",
+    github: "https://github.com/Saobia3i",
+    imageUrl: "https://drive.google.com/open?id=1iQGd",
+  },
+  // Other Executives
+  {
+    id: 2,
+    name: "Samanta Islam",
+    designation: "Deputy Executive",
+    email: "samanta.cse.202301040",
+    linkedin: "https://www.linkedin.com/in",
+    facebook: "https://www.facebook.com/sam",
+    github: "https://github.com/Samanta",
+    imageUrl: "https://drive.google.com/open?id=1THVw",
+  },
+  {
+    id: 3,
+    name: "Farhana Rahman",
+    designation: "Senior Sub Executive",
+    email: "farhanarahan7361@gm",
+    linkedin: "www.linkedin.com/in/farhan",
+    facebook: "https://www.facebook.com/sha",
+    github: "https://github.com/Farhana7",
+    imageUrl: "https://drive.google.com/open?id=1y_NZ",
+  },
+  {
+    id: 4,
+    name: "Mohammad Obaidul Ekr",
+    designation: "Senior Sub Executive",
+    email: "riadekram410@gmail.coi",
+    linkedin: "https://www.linkedin.com/in",
+    facebook: "https://www.facebook.com/sha",
+    github: "https://github.com/riadekram",
+    imageUrl: "https://drive.google.com/open?id=15XH6",
+  },
+  {
+    id: 5,
+    name: "Rohan Rahim",
+    designation: "Sub Executive",
+    email: "rohanrahim024@gmail.co",
+    linkedin: "https://www.linkedin.com/in",
+    facebook: "https://www.facebook.com/roh",
+    github: "https://github.com/rohan993",
+    imageUrl: "https://drive.google.com/open?id=19Bwa",
+  },
+  {
+    id: 6,
+    name: "Nushrat Yeasmin Nahin",
+    designation: "Sub Executive",
+    email: "nushrat.cse.2023010400",
+    linkedin: "https://www.linkedin.com/in",
+    facebook: "https://www.facebook.com/sha",
+    github: "https://github.com/nushratnahin",
+    imageUrl: "https://drive.google.com/open?id=1neqrR",
+  },
+];
